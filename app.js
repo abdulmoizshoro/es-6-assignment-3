b09daffec133574759ecabcde84fddfd956794a6
Abdul Moiz Shoro <abdulmoizshoro@gmail.com>
2021-03-29 09:43:33 -0700

    work compelete

---
+++ app.js
@@ -0,0 +1,52 @@
+//1- How to unique, sort an array in ascending order?
+// const arr = [1,40,2,10,10,40,2,2,5,3,54,2,42,5,5,5]
+
+// const arr = [1,40,2,10,10,40,2,2,5,3,54,2,42,5,5,5]
+// console.log(arr.sort(function(a, b){return a-b}))
+
+
+// 2- Print values using for loop in the following sequence
+// 1
+// 123
+// 1234
+// 12345
+
+// const arr = [1,12,123,1234,12345]
+// const newArr = new Set(arr)
+
+// for(let o of newArr){
+//     console.log(o);
+// }
+
+
+// 3- Sum the folliwg number using for loop
+// const num = 12345 (its number type)
+// Output should be: 15
+
+
+// var num = 12345;
+// var sum = 0;
+
+// while(num != 0){
+//     sum += num % 10;
+//   num = parseInt( num / 10 );
+   
+// }
+// console.log(sum);
+
+
+// 4- Find the third highest number in array using Set, sort and reverse
+// const arr = [1,210,5,7,65,5,20,8,210,65];
+
+// const arr = [1,210,5,7,65,5,20,8,210,65];
+
+// let sortArr = arr.sort(function(a, b){return a - b})
+
+// let arrInDescendingOrder = sortArr.reverse()
+
+// let setArr = new Set(arrInDescendingOrder)
+
+// console.log(setArr)
+
+
+
